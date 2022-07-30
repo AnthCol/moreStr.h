c0420808bdb7667695f69adc2d4ad251d8cbb591
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2022-07-30 01:04:04 -0400

    added many functions

--- string2.h
+++ string2.h
@@ -1,15 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdbool.h>
 
 char * substring(const char * input, int leftIndex, int rightIndex){
+    if (leftIndex > rightIndex || leftIndex < 0 || input == NULL) exit(0); 
+
     int i, x = 0, length = 0; 
-    for (i = 0; input[i] != '\0'; i++){
+
+    for (i = 0; input[i] != '\0'; i++){ // I'm sure strlen(input) could be fine here, but I wanted to make it myself. 
         length++; 
     }
     
-    if (leftIndex > rightIndex || leftIndex < 0 || input == NULL) exit(0); 
     if (rightIndex > length || leftIndex > length || rightIndex - leftIndex == 0) exit(0); 
 
     char * newString = (char*)malloc(sizeof(char) * (rightIndex - leftIndex)); 
@@ -22,4 +23,70 @@ char * substring(const char * input, int leftIndex, int rightIndex){
 
     return newString; 
 }
+
+bool endsWith(const char * input, char check){
+    int i = 0; 
+
+    while (input[i] != '\0'){
+        i++; 
+    }
+
+    if (input[i-1] == check) return true; 
+    return false; 
+}
+
+bool isEmpty (const char * input){
+    int length = 0; 
+
+    while (input[length] != '\0'){
+        length++; 
+    }
+
+    if (length == 0) return true;
+    return false; 
+}
+
+bool startsWith(const char * input, const char * check){
+    int i = 0, x = 0; 
+    while (input[i] != '\0'){
+        i++; 
+    }
+    while (check[x] != '\0'){
+        x++; 
+    }
+    if (x > i) exit(0); 
+
+    i = 0; 
+
+    while (check[i] != '\0'){
+        if (input[i] != check[i]) return false; 
+        i++; 
+    }
+
+    return true; 
+}
+
+
+
+/* Do these two functions below by hand. Get rid of the ctype.h reliance */
+
+
+char * toLowerCase(char * input){  
+    for (int i = 0; input[i] != '\0'; i++){
+        if (isupper(input[i])){
+            tolower(input[i]); 
+        }
+    }
+    return input; 
+}
+
+char * toUpperCase(char * input){
+    for (int i = 0; input[i] != '\0'; i++){
+        if (islower(input[i])){
+            toupper(input[i]); 
+        }
+    }
+    return input; 
+}
+
 
--- testMain.c
+++ testMain.c
@@ -1,12 +1,14 @@
 #include "string2.h"
+#include <stdio.h>
 
 int main(void){
 
     char * string = "testing"; 
-    char * newString = substring(string, 1, 5); 
+    char * new = substring(string, 1, 5); 
 
-    printf("printing new string %s\n", newString);
-    free(newString); 
+    printf("printing new string from substring: %s\n", new); 
+    free(new); 
+    printf("checking endsWith: %d\n", endsWith(string, 'g')); 
 
     return 0; 
 }
